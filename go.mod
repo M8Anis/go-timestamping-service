f83f289da38dec1f2c9c85c6de3073226cc6fcd4
Elisey (Mooanis) Polikarpov <mooanis@inbox.ru>
2024-08-31 10:10:54 +0300

    Adding RFC3161 timestamping

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module gitea.m8anis.internal/M8Anis/go-timestamping-service
 
 go 1.23.0
 
-require github.com/github/smimesign v0.2.0 // direct
+require (
+	github.com/digitorus/timestamp v0.0.0-20231217203849-220c5c2851b7 // direct
+	github.com/github/smimesign v0.2.0 // direct
+)
+
+require (
+	github.com/certifi/gocertifi v0.0.0-20210507211836-431795d63e8d // indirect
+	github.com/digitorus/pkcs7 v0.0.0-20230818184609-3a137a874352 // indirect
+	github.com/pborman/getopt v1.1.0 // indirect
+	github.com/pborman/getopt/v2 v2.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+)
--- service/rfc3161.go
+++ service/rfc3161.go
@@ -1,9 +1,50 @@
 package service
 
 import (
+	"crypto"
+	"encoding/asn1"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/digitorus/timestamp"
 )
 
 func Rfc3161(w http.ResponseWriter, body []byte) {
-	ErrorPage(w, http.StatusInternalServerError, "RFC3161 not implemented")
+	req, err := timestamp.ParseRequest(body)
+	if err != nil {
+		ErrorPage(w, http.StatusInternalServerError,
+			"Error has occured. For more information, refer to the console",
+		)
+		log.Printf("Body cannot be parsed: %s", err)
+		return
+	}
+
+	ts := timestamp.Timestamp{
+		HashAlgorithm: req.HashAlgorithm,
+		HashedMessage: req.HashedMessage,
+
+		Nonce: req.Nonce,
+
+		Time:     time.Now().UTC(),
+		Accuracy: time.Millisecond * 100,
+
+		AddTSACertificate: true,
+
+		Qualified: true,
+		Ordering:  true,
+		Policy:    asn1.ObjectIdentifier{2, 4, 5, 6},
+	}
+	resp, err := ts.CreateResponseWithOpts(signingCertificate, signingKey, crypto.SHA256)
+	if err != nil {
+		ErrorPage(w, http.StatusInternalServerError,
+			"Error has occured. For more information, refer to the console",
+		)
+		log.Printf("Response cannot be created: %s", err)
+		return
+	}
+
+	w.Header().Add("Content-Type", RFC3161_REPLY)
+	fmt.Fprintf(w, "%s", resp)
 }
