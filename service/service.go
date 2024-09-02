bc2fd2de079a55b90be55e34da6ee441ca7afb85
Elisey (Mooanis) Polikarpov <mooanis@inbox.ru>
2024-09-02 06:46:35 +0300

    Changing the read encoding to PEM

--- service/service.go
+++ service/service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"crypto/ecdsa"
 	"crypto/x509"
+	"encoding/pem"
 	"log"
 	"os"
 )
@@ -11,20 +12,28 @@ var signingKey *ecdsa.PrivateKey
 var signingCertificate *x509.Certificate
 
 func init() {
-	key, err := os.ReadFile("./certs/ts-key.der")
+	pemKey, err := os.ReadFile("./certs/ts-key.pem")
 	if err != nil {
 		log.Fatalf("Private key cannot be read: %s", err)
 	}
-	signingKey, err = x509.ParseECPrivateKey(key)
+	key, _ := pem.Decode(pemKey)
+	if key == nil {
+		log.Fatalf("Private key cannot be decoded: %s", err)
+	}
+	signingKey, err = x509.ParseECPrivateKey(key.Bytes)
 	if err != nil {
 		log.Fatalf("EC private key cannot be parsed: %s", err)
 	}
 
-	cert, err := os.ReadFile("./certs/ts-crt.der")
+	pemCert, err := os.ReadFile("./certs/ts-crt.pem")
 	if err != nil {
 		log.Fatalf("Certificate cannot be read: %s", err)
 	}
-	signingCertificate, err = x509.ParseCertificate(cert)
+	cert, _ := pem.Decode(pemCert)
+	if key == nil {
+		log.Fatalf("Certificate cannot be decoded: %s", err)
+	}
+	signingCertificate, err = x509.ParseCertificate(cert.Bytes)
 	if err != nil {
 		log.Fatalf("Certificate cannot be parsed: %s", err)
 	}
